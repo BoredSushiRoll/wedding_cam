6aea581fb49d429b303e1181b7ca89bdc05bbba8
Rares Dragan <rares.dragan42@gmail.com>
2026-08-31 15:24:35 +0300

    UI Final Version, Backend Functional

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Bypasses the local LAN firewall for your phone
+  allowedDevOrigins: ['192.168.0.89']
 };
 
 export default nextConfig;
